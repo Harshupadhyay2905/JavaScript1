44edbab469b9955f8dc80b6c8bc18f15434f5445
Harsh Kumar Upadhyay <96164726+Harshupadhyay2905@users.noreply.github.com>
2024-01-21 20:03:34 +0000

    functions

---
+++ 01_basics/functions.js
@@ -0,0 +1,19 @@
+//function addTwoNumbers(number1,number2)
+//{
+ //   return number1+number2
+//}
+//const result=addTwoNumbers(3,5)
+//console.log(result)//
+            //function loginUserMessage(username){
+            //    if(username=== undefined){
+            //        console.log("please enter the username")
+            //   }
+            //   return `${username} just logged in`
+            //}
+            //
+            //console.log(loginUserMessage("hitesh"))
+
+            function calculateCartPrice(...num1){
+                return num1;
+            }
+            console.log(calculateCartPrice(200,400,500))
